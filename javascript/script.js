9f44793cad30ccf53730abfa87b30bfb8056f563
esha-kewalramani <eshakewalramani562@gmail.com>
2026-05-15 13:50:58 +0500

    first commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,30 +1,14 @@
-// let rows=5;
-// for (i=1; i<=5; i++)
-// {
-// let output="";
-// for(j=1; j<=rows-i; j++)
-// {
-//     output+=" ";
-// }
-// for(k=1; k>=i; k--)
-// {
-//     output+= k;
-// }
-// for(d=2; d<=i; d++)
-// {
-//     output+= d;
-// }
-// console.log(output);
-// }
-let a="hello";
-let b= "world";
-let c=a.concat(" ",b);
-console.log(c);
- a="hello";
- b= "world";
- c=a.concat(b);
-console.log(c);
- a="hello";
- b= "world";
- c=a.concat(b);
-console.log(c);
+let rows=5;
+for (i=1; i<=5; i++){
+let output="";
+for(j=1; j<=rows-i; j++){
+    output+="  ";
+}
+for(k=1; k>=i; k--){
+    output+= k+" ";
+}
+for(d=2; d<=i; d++){
+    output+= d+" ";
+}
+console.log(output);
+}
